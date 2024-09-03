aa79255b6d20e9c67ec06205d0f5398ee501264a
Stefan Kondratschin <kondratschin@gmail.com>
2024-09-03 23:11:01 +0200

    sounds

--- models/character.class.js
+++ models/character.class.js
@@ -11,6 +11,7 @@ class Character extends MovableObject {
         ];
     
     world;
+    walking_sound = new Audio('audio/walking.mp3');
 
     constructor() {
         super().loadImage("img/2_character_knight/2_walk/walk1.png");
@@ -22,16 +23,19 @@ class Character extends MovableObject {
    animate() {
 
         setInterval(() => {
-            if (this.world.keyboard.RIGHT) {
+            this.walking_sound.pause();
+            if (this.world.keyboard.RIGHT && this.x < this.world.level.level_end_x) {
                 this.x += this.speed;
                 this.otherDirection = false;
+                this.walking_sound.play();
         }
 
-        if (this.world.keyboard.LEFT) {
+        if (this.world.keyboard.LEFT && this.x > 0) {
             this.x -= this.speed;
             this.otherDirection = true;
+            this.walking_sound.play();
         }
-        this.world.camera_x = -this.x + 100;
+        this.world.camera_x = -this.x + 50;
     }, 1000 / 60);
 
         setInterval(() => {
