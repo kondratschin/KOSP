63cbc02df39b96761673794794fd1d9496fac3f4
Kondratschin <Stefan.Kondratschin@spray.com>
2024-09-03 08:45:28 +0200

    added level class

---
+++ levels/level1.js
@@ -0,0 +1,31 @@
+const level1 = new Level(
+    [
+        new Enemy(),
+        new Enemy(),
+        new Enemy(),
+    ],
+
+    [
+        new Cloud(0, -100),
+        new Cloud(720, -100), // Second cloud at a different position
+    ],
+
+    [
+        new BackgroundObject('img/5_background/layers/5_clouds/clouds1.png', 0, 0, 720, 480),
+        new BackgroundObject('img/5_background/layers/3_third_layer/clouds2.png', 0, 0, 720, 480),
+        new BackgroundObject('img/5_background/layers/3_third_layer/rocks2.png', 0, 0, 720, 480),
+        new BackgroundObject('img/5_background/layers/2_second_layer/rocks3.png', 0, 0, 720, 480),
+        new BackgroundObject('img/5_background/layers/1_first_layer/02_ground.png', 0, 414, 64, 64),
+        new BackgroundObject('img/5_background/layers/1_first_layer/02_ground.png', 64, 414, 64, 64),
+        new BackgroundObject('img/5_background/layers/1_first_layer/02_ground.png', 128, 414, 64, 64),
+        new BackgroundObject('img/5_background/layers/1_first_layer/02_ground.png', 192, 414, 64, 64),
+        new BackgroundObject('img/5_background/layers/1_first_layer/02_ground.png', 256, 414, 64, 64),
+        new BackgroundObject('img/5_background/layers/1_first_layer/02_ground.png', 320, 414, 64, 64),
+        new BackgroundObject('img/5_background/layers/1_first_layer/02_ground.png', 384, 414, 64, 64),
+        new BackgroundObject('img/5_background/layers/1_first_layer/02_ground.png', 448, 414, 64, 64),
+        new BackgroundObject('img/5_background/layers/1_first_layer/02_ground.png', 512, 414, 64, 64),
+        new BackgroundObject('img/5_background/layers/1_first_layer/02_ground.png', 576, 414, 64, 64),
+        new BackgroundObject('img/5_background/layers/1_first_layer/02_ground.png', 640, 414, 64, 64),
+        new BackgroundObject('img/5_background/layers/1_first_layer/02_ground.png', 704, 414, 64, 64),
+    ]
+);
--- models/world.class.js
+++ models/world.class.js
@@ -1,34 +1,8 @@
 class World {
     character = new Character();
-    enemies = [
-        new Enemy(),
-        new Enemy(),
-        new Enemy(),
-    ];
-    clouds = [
-        new Cloud(0, -100),
-        new Cloud(720, -100), // Second cloud at a different position
-    ];
-    backgroundObjects = [
-
-        new BackgroundObject('img/5_background/layers/5_clouds/clouds1.png', 0, 0, 720, 480),
-        new BackgroundObject('img/5_background/layers/3_third_layer/clouds2.png', 0, 0, 720, 480),
-        new BackgroundObject('img/5_background/layers/3_third_layer/rocks2.png', 0, 0, 720, 480),
-        new BackgroundObject('img/5_background/layers/2_second_layer/rocks3.png', 0, 0, 720, 480),
-        new BackgroundObject('img/5_background/layers/1_first_layer/02_ground.png', 0, 414, 64, 64),
-        new BackgroundObject('img/5_background/layers/1_first_layer/02_ground.png', 64, 414, 64, 64),
-        new BackgroundObject('img/5_background/layers/1_first_layer/02_ground.png', 128, 414, 64, 64),
-        new BackgroundObject('img/5_background/layers/1_first_layer/02_ground.png', 192, 414, 64, 64),
-        new BackgroundObject('img/5_background/layers/1_first_layer/02_ground.png', 256, 414, 64, 64),
-        new BackgroundObject('img/5_background/layers/1_first_layer/02_ground.png', 320, 414, 64, 64),
-        new BackgroundObject('img/5_background/layers/1_first_layer/02_ground.png', 384, 414, 64, 64),
-        new BackgroundObject('img/5_background/layers/1_first_layer/02_ground.png', 448, 414, 64, 64),
-        new BackgroundObject('img/5_background/layers/1_first_layer/02_ground.png', 512, 414, 64, 64),
-        new BackgroundObject('img/5_background/layers/1_first_layer/02_ground.png', 576, 414, 64, 64),
-        new BackgroundObject('img/5_background/layers/1_first_layer/02_ground.png', 640, 414, 64, 64),
-        new BackgroundObject('img/5_background/layers/1_first_layer/02_ground.png', 704, 414, 64, 64),
-    ];
-
+    enemies = level1.enemies;
+    clouds = level1.clouds;
+    backgroundObjects = level1.backgroundObjects;
     canvas;
     ctx;
     keyboard;
