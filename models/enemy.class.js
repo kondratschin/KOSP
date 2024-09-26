3e5e558cc03b862bbe71ec1789f8dc614544afe5
Stefan Kondratschin <kondratschin@gmail.com>
2024-09-26 21:36:34 +0200

    images

--- models/enemy.class.js
+++ models/enemy.class.js
@@ -9,10 +9,23 @@ class Enemy extends MovableObject {
         'img/3_enemies/snake/1_walk/Walk3.png',
         'img/3_enemies/snake/1_walk/Walk4.png'
     ];
+    IMAGES_DEAD = [
+        'img/3_enemies/snake/2_dead/Death1.png',
+        'img/3_enemies/snake/2_dead/Death2.png',
+        'img/3_enemies/snake/2_dead/Death3.png',
+        'img/3_enemies/snake/2_dead/Death4.png'
+    ];
+
+    IMAGES_HURT = [
+        'img/3_enemies/snake/3_hurt/Hurt1.png',
+        'img/3_enemies/snake/3_hurt/Hurt2.png'
+    ];
 
     constructor() {
         super().loadImage("img/3_enemies/snake/1_walk/Walk1.png");
         this.loadImages(this.IMAGES_WALKING);
+        this.loadImages(this.IMAGES_DEAD);
+        this.loadImages(this.IMAGES_HURT);
         this.x = 200 + Math.random() * 500;
         this.speed = 0.3 + Math.random() * 0.5;
         this.animate();
