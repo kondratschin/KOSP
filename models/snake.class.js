205c31404d1fad10c61ce4be1aa9dc30b408fbb1
Kondratschin <Stefan.Kondratschin@spray.com>
2024-10-10 17:36:55 +0200

    jsdoc snake

--- models/snake.class.js
+++ models/snake.class.js
@@ -1,5 +1,4 @@
 class Snake extends MovableObject {
-    // Initial properties
     y = 314;
     height = 180;
     width = 180;
@@ -9,7 +8,6 @@ class Snake extends MovableObject {
     snakeDead = new Audio('audio/snake_dead.mp3');
     snakeHit = new Audio('audio/enemy_hit.mp3');
 
-    // Image arrays for different states
     IMAGES_WALKING = [
         'img/3_enemies/snake/1_walk/Walk1.png',
         'img/3_enemies/snake/1_walk/Walk2.png',
@@ -27,54 +25,90 @@ class Snake extends MovableObject {
         'img/3_enemies/snake/3_hurt/Hurt2.png'
     ];
 
+    /**
+     * Create a snake.
+     */
     constructor() {
         super().loadImage("img/3_enemies/snake/1_walk/Walk1.png");
         this.loadImages(this.IMAGES_WALKING);
         this.loadImages(this.IMAGES_DEAD);
         this.loadImages(this.IMAGES_HURT);
-        this.x = 400 + Math.random() * 500; // Random initial x position
-        this.speed = 0.3 + Math.random() * 0.5; // Random speed
+        this.x = 400 + Math.random() * 500;
+        this.speed = 0.3 + Math.random() * 0.5;
         this.animate();
     }
 
-    // Start animation and movement intervals
+    /**
+     * Start animation and movement intervals.
+     */
     animate() {
-        this.movementInterval = setInterval(() => {
-            this.moveLeft();
-        }, 1000 / 60); // 60 FPS movement
-
-        this.animationInterval = setInterval(() => {
-            this.moveEnemy();
-        }, 200); // Adjust the interval as needed
+        this.startMovement();
+        this.startAnimation();
     }
 
-    // Handle enemy movement and state changes
+    /**
+     * Start the movement interval.
+     */
+    startMovement() {
+        this.movementInterval = setInterval(() => {
+            this.moveLeft();
+        }, 1000 / 60);
+    }
+
+    /**
+     * Start the animation interval.
+     */
+    startAnimation() {
+        this.animationInterval = setInterval(() => {
+            this.moveEnemy();
+        }, 200);
+    }
+
+    /**
+     * Handle enemy movement and state changes.
+     */
     moveEnemy() {
         if (this.isDead()) {
-            if (!soundMute) {
-                this.snakeDead.play();
-            }
-            this.playAnimationOnce(this.IMAGES_DEAD);
-            this.speed = 0;
-            this.stopAllIntervals();
-            setTimeout(() => {
-                this.removeCorpse = true;
-            }, 1500); // Remove corpse after 1.5 seconds
+            this.handleDeath();
         } else if (this.isHurt()) {
-            if (!soundMute) {
-                this.snakeHit.play();
-            }
-            this.playAnimationOnce(this.IMAGES_HURT);
-            this.speed = 0;
-            setTimeout(() => {
-                this.speed = 0.3 + Math.random() * 0.5; // Reset speed after hurt
-            }, 500);
+            this.handleHurt();
         } else {
             this.playAnimation(this.IMAGES_WALKING);
         }
     }
 
-    // Stop animation interval
+    /**
+     * Handle the snake's death.
+     */
+    handleDeath() {
+        if (!soundMute) {
+            this.snakeDead.play();
+        }
+        this.playAnimationOnce(this.IMAGES_DEAD);
+        this.speed = 0;
+        this.stopAllIntervals();
+        setTimeout(() => {
+            this.removeCorpse = true;
+        }, 1500);
+    }
+
+    /**
+     * Handle the snake being hurt.
+     */
+    handleHurt() {
+        if (!soundMute) {
+            this.snakeHit.play();
+        }
+        this.playAnimationOnce(this.IMAGES_HURT);
+        this.speed = 0;
+        setTimeout(() => {
+            this.speed = 0.3 + Math.random() * 0.5;
+        }, 500);
+    }
+
+    /**
+     * Stop the animation interval.
+     */
     stopAnimationInterval() {
         if (this.animationInterval) {
             clearInterval(this.animationInterval);
@@ -82,7 +116,9 @@ class Snake extends MovableObject {
         }
     }
 
-    // Stop movement interval
+    /**
+     * Stop the movement interval.
+     */
     stopMovementInterval() {
         if (this.movementInterval) {
             clearInterval(this.movementInterval);
@@ -90,7 +126,9 @@ class Snake extends MovableObject {
         }
     }
 
-    // Stop all intervals
+    /**
+     * Stop all intervals.
+     */
     stopAllIntervals() {
         this.stopAnimationInterval();
         this.stopMovementInterval();
